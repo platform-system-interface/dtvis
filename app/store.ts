237daadb8dfaa6a8b809e5166315733d41d82975
m0veax <patrick.kilter@gmail.com>
2024-01-30 18:46:30 +0100

    get a template by CPDD

--- app/store.ts
+++ app/store.ts
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import treeReducer from './features/tree/treeSlice';
 
 export const makeStore = () => {
   return configureStore({
@@ -11,3 +12,9 @@ export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
