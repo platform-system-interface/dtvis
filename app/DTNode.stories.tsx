b8cbd849d852ce5b8f5ddd7a9bc8e97a65658522
Daniel Maslowski <info@orangecms.org>
2024-08-20 17:29:42 +0200

    DTNode: add Storybook story

---
+++ app/DTNode.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { DataNode } from './DTNode';
+
+const meta = {
+  title: 'App/DTNode',
+  component: DataNode,
+  argTypes: {
+    data: {
+      label: { control: 'text' },
+      baseAddr: {},
+      compat: {},
+    },
+    status: { control: 'radio', options: ['okay', 'disabled'] },
+  },
+} satisfies Meta<typeof DataNode>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Simple: Story = {
+  args: {
+    data: {
+      label: "UART",
+      baseAddr: "0x0c00_0000",
+    },
+    status: "okay",
+  },
+};
+
+export const WithCompat: Story = {
+  args: {
+    data: {
+      label: "firmware",
+      compat: "raspberrypi,bcm2835-firmware",
+    },
+  },
+};
