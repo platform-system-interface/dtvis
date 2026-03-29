cf0cc310544565e62f27da7d20839ad1fcfdb849
Daniel Maslowski <info@orangecms.org>
2026-03-29 10:41:17 +0200

    WIP: quick and dirty, FIT image props, phandles
    Unfortunately, there is no field indicating that it is a FIT.
    Neither is there type information in the binary, so we "know".
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- app/DTNode.tsx
+++ app/DTNode.tsx
@@ -81,15 +81,43 @@ const Compat: FC<{ compat?: string }> = ({ compat }) => {
 };
 
 export const Extra: FC<{ data: DTNodeData }> = ({ data }) => {
-  const { resets, clks, phandle, phySupply, phyHandle } = data;
+  const { resets, clocks, mboxes, phandle, phySupply, phyHandle } = data;
+  const { description, type, arch, os, kernel, compression, load, entry } =
+    data;
 
   const e = JSON.stringify(
-    { resets, clks, phandle, phySupply, phyHandle },
+    {
+      resets,
+      clocks,
+      mboxes,
+      phandle,
+      phySupply,
+      phyHandle,
+    },
+    null,
+    2,
+  );
+  const x = JSON.stringify(
+    {
+      description,
+      type,
+      arch,
+      os,
+      kernel,
+      compression,
+      load,
+      entry,
+    },
     null,
     2,
   );
 
-  return <div>{e}</div>;
+  return (
+    <div>
+      {e}
+      {x}
+    </div>
+  );
 };
 
 export const DataNode: FC<{ data: DTNodeData; status?: DTStatus }> = ({
