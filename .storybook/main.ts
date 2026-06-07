3cfea45a3a3760496540b0a8db759f1aad528c6e
지지 ᚠד (Jiji Freya Daniel) Maslowski <info@orangecms.org>
2026-06-07 20:16:10 +0200

    fix up Storybook setup
    Signed-off-by: 지지 ᚠד (Jiji Freya Daniel) Maslowski <info@orangecms.org>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,8 +7,6 @@ const config: StorybookConfig = {
   ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/nextjs",
