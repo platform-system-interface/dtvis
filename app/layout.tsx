2603bd78ff90addd5ac9d1a053178e9d06a52a49
지지 ᚠד (Jiji Freya Daniel) Maslowski <info@orangecms.org>
2026-09-20 18:14:17 +0200

    migrate from reactflow (v11) to @xyflow/react (v12)
    Signed-off-by: 지지 ᚠד (Jiji Freya Daniel) Maslowski <info@orangecms.org>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "reactflow/dist/style.css";
+import "@xyflow/react/dist/style.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
